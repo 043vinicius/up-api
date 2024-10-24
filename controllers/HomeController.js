cc0231044cd01bd3195ddabec82602e363541a11
Vinicius Moreira <vinicius.moreira.silva557@gmail.com>
2024-10-24 19:22:58 -0300

    added jsdocs

--- controllers/HomeController.js
+++ controllers/HomeController.js
@@ -1,5 +1,10 @@
 class HomeController{
 
+    /**
+     * Página inicial para validar funcionamento da API
+     * @param {*} req 
+     * @param {*} res 
+     */
     async index(req, res){
         res.send("Api esta funcionando");
     }
--- controllers/UserController.js
+++ controllers/UserController.js
@@ -6,6 +6,12 @@ const bcrypt = require("bcrypt");
 
 
 class UserController {
+
+    /**
+     * Rota para listar todos os usuários
+     * @param {*} req 
+     * @param {*} res 
+     */
     async index(req, res) {
         const users = await User.findAll();
         res.json({
@@ -14,15 +20,19 @@ class UserController {
         });
     }
 
+    /**
+     * Rota para criar um novo usuário
+     * @param {*} req
+     * @param {*} res
+     * @return {Object} Retorna um JSON com status da operação e os dados do usuário criado
+     */
     async create(req, res) {
         const { email, name, password } = req.body;
-
         if (email == undefined) {
             res.status(400);
             res.json({ err: "O e-mail é inválido!" })
             return;
         }
-
         const emailExists = await User.findEmail(email);
 
         if (emailExists) {
@@ -31,9 +41,7 @@ class UserController {
             return;
         }
 
-
         await User.new(email, password, name);
-
         res.status(200);
         res.json({
             status: true,
@@ -43,30 +51,30 @@ class UserController {
         });
     }
 
+    /**
+     * Rota para autenticar um usuário
+     * @param {*} req
+     * @param {*} res
+     * @return {Object} Retorna um JSON com status da operação e o token de autenticação
+     */
     async login(req, res) {
         const { email, password } = req.body;
-
         const user = await User.findByEmail(email);
 
-        if (user != undefined) {
-
+        if (user) {
             const resultado = await bcrypt.compare(password, user.password);
-
             if (resultado) {
                 const token = jwt.sign({ email: user.email }, secret);
 
                 res.status(200);
                 res.json({ token: token });
-
             } else {
                 res.status(400);
                 res.send("Senha incorreta");
             }
-
         } else {
             res.status(404);
             res.json({ status: false, message: "Usuário não encontrado" });
-
         }
     }
 
